69f578528aa51ffd65ce4993bd93efe275b3ae21
jwzr <38551546+jwzr@users.noreply.github.com>
2018-04-20 16:48:18 +0800

    Update HelloWorld.java
    my first update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,14 @@
 public class HelloWorld{
   private String usa;
+  private String china;
+  
+  public void setChina(String china) {
+    this.china = china;
+  }
+  
+  public String getChina(){
+    return this.china;
+  }
   
   public void setUsa(String usa) {
     this.usa = usa;
